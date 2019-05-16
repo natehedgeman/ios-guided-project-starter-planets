071e0e66beb9aad80f23e502f776bcca62824e1f
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-05-15 22:11:13 -0400

    Set up interface for planets

---
+++ Planets/Model Controller/PlanetCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  PlanetCollectionViewCell.swift
+//  Planets
+//
+//  Created by Nathan Hedgeman on 5/15/19.
+//  Copyright © 2019 Lambda Inc. All rights reserved.
+//
+
+import UIKit
+
+class PlanetCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
+    
+}
